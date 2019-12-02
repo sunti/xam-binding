e9f905beceeb752f10643cb207db49a352a9198a
sunti <sunti@codefin.io>
2019-12-02 10:24:52 +0700

    android init

---
+++ AndroidBinding/Additions/ReactInstanceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Com.Facebook.React
+{
+    public partial class ReactInstanceManager
+    {
+        /// <summary>
+        /// Initializes the <see cref="T:Com.Facebook.React.ReactInstanceManager"/> class.
+        /// Load necessary jni libraries via static ctor.
+        /// </summary>
+        static ReactInstanceManager()
+        {
+            // Load jni libraries.
+            Java.Lang.JavaSystem.LoadLibrary("imagepipeline");
+            Java.Lang.JavaSystem.LoadLibrary("reactnativejni");
+
+            Java.Lang.JavaSystem.LoadLibrary("c++_shared");
+            Java.Lang.JavaSystem.LoadLibrary("fb");
+            Java.Lang.JavaSystem.LoadLibrary("folly_futures");
+            Java.Lang.JavaSystem.LoadLibrary("folly_json");
+            Java.Lang.JavaSystem.LoadLibrary("glog");
+            Java.Lang.JavaSystem.LoadLibrary("glog_init");
+            Java.Lang.JavaSystem.LoadLibrary("jsc");
+            Java.Lang.JavaSystem.LoadLibrary("jscexecutor");
+            Java.Lang.JavaSystem.LoadLibrary("jsinspector");
+            Java.Lang.JavaSystem.LoadLibrary("native-filters");
+            Java.Lang.JavaSystem.LoadLibrary("native-imagetranscoder");
+            Java.Lang.JavaSystem.LoadLibrary("reactnativeblob");
+            Java.Lang.JavaSystem.LoadLibrary("yoga");
+        }
+    }
+}
--- DependencyServiceDemos.Android/RNActivity.cs
+++ DependencyServiceDemos.Android/RNActivity.cs
@@ -11,16 +11,53 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+using Com.Facebook.React;
+//using Com.Facebook.React.Shell;
+//using Com.Facebook.React.Common;
+//using Com.Facebook.React.Modules.Core;
+using Android.Support.V7.App;
+using Android.Net;
+using Android.Provider;
+
 namespace DependencyServiceDemos.Droid
 {
     [Activity(Label = "RNActivity")]
     public class RNActivity : Activity
     {
+        RCTRootView mReactRootView;
+        ReactInstanceManager mReactInstanceManager;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Create your application here
+            //Create your application here
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                if (!Settings.CanDrawOverlays(this))
+                {
+                    Intent intent = new Intent(Settings.ActionManageOverlayPermission,Android.Net.Uri.Parse("package:" + PackageName));
+                    StartActivityForResult(intent,1);
+                }
+            }
+
+            mReactRootView = new ReactRootView(this);
+            mReactInstanceManager = ReactInstanceManager.Builder()
+                    .SetApplication(Application)
+                    .SetBundleAssetName("index.android.bundle")
+                    .SetJSMainModulePath("index")
+                    .AddPackage(new MainReactPackage())
+#if DEBUG
+                    .SetUseDeveloperSupport(true)
+#else
+                    .SetUseDeveloperSupport(false)
+#endif
+                    .SetInitialLifecycleState(LifecycleState.Resumed)
+                    .Build();
+
+            mReactRootView.StartReactApplication(mReactInstanceManager, "xxxx", null);
+
+            SetContentView(mReactRootView);
         }
     }
 }
