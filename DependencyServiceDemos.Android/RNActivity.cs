fb4b4445b28aa6042c8588b088363c18fcb5dfbc
sunti <sunti@codefin.io>
2019-12-09 10:49:45 +0700

    fix jsc

--- DependencyServiceDemos.Android/RNActivity.cs
+++ DependencyServiceDemos.Android/RNActivity.cs
@@ -12,9 +12,9 @@ using Android.Views;
 using Android.Widget;
 
 using Com.Facebook.React;
-//using Com.Facebook.React.Shell;
-//using Com.Facebook.React.Common;
-//using Com.Facebook.React.Modules.Core;
+using Com.Facebook.React.Shell;
+using Com.Facebook.React.Common;
+using Com.Facebook.React.Modules.Core;
 using Android.Support.V7.App;
 using Android.Net;
 using Android.Provider;
@@ -24,7 +24,7 @@ namespace DependencyServiceDemos.Droid
     [Activity(Label = "RNActivity")]
     public class RNActivity : Activity
     {
-        RCTRootView mReactRootView;
+        ReactRootView mReactRootView;
         ReactInstanceManager mReactInstanceManager;
 
         protected override void OnCreate(Bundle savedInstanceState)
