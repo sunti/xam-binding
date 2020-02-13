e8d8aa78aa641a6686c4dc0d875d963a1cd6ec48
sunti <sunti@codefin.io>
2020-02-13 15:49:00 +0700

    fix ref

---
+++ AndroidBinding/Additions/DevInternalSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.Content;
+
+namespace Com.Facebook.React.Devsupport
+{
+    public partial class DevInternalSettings
+    {
+
+        public void OnSharedPreferenceChanged(ISharedPreferences pref, string data)
+        {
+            return;
+        }
+
+    }
+}
--- AndroidBinding/Additions/ReactInstanceManager.cs
+++ AndroidBinding/Additions/ReactInstanceManager.cs
@@ -10,23 +10,9 @@ namespace Com.Facebook.React
         /// </summary>
         static ReactInstanceManager()
         {
-            // Load jni libraries.
             Java.Lang.JavaSystem.LoadLibrary("imagepipeline");
             Java.Lang.JavaSystem.LoadLibrary("reactnativejni");
 
-            Java.Lang.JavaSystem.LoadLibrary("c++_shared");
-            Java.Lang.JavaSystem.LoadLibrary("fb");
-            Java.Lang.JavaSystem.LoadLibrary("folly_futures");
-            Java.Lang.JavaSystem.LoadLibrary("folly_json");
-            Java.Lang.JavaSystem.LoadLibrary("glog");
-            Java.Lang.JavaSystem.LoadLibrary("glog_init");
-            Java.Lang.JavaSystem.LoadLibrary("jsc");
-            Java.Lang.JavaSystem.LoadLibrary("jscexecutor");
-            Java.Lang.JavaSystem.LoadLibrary("jsinspector");
-            Java.Lang.JavaSystem.LoadLibrary("native-filters");
-            Java.Lang.JavaSystem.LoadLibrary("native-imagetranscoder");
-            Java.Lang.JavaSystem.LoadLibrary("reactnativeblob");
-            Java.Lang.JavaSystem.LoadLibrary("yoga");
         }
     }
 }
